6839e3960891d8e8d5fdfb0a58cf0ce070129e9a
khadijayounas12 <151490125+khadijayounas12@users.noreply.github.com>
2023-11-26 17:08:32 +0500

    Add files via upload

---
+++ Prog 5.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Prog5{ 
+public static void main(String[] args){
+Scanner s=new Scanner(System.in);
+System.out.println("Enter temp in celsius");
+double celsius=s.nextDouble();
+double fahrenheit=(celsius*9/5)+32;
+System.out.println("celsius to fahrenheit" +fr);
+}
+
+
+}
