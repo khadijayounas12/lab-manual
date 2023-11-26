4bee7914db6fcee0787e52d6913399fa04f77b77
khadijayounas12 <151490125+khadijayounas12@users.noreply.github.com>
2023-11-26 17:07:14 +0500

    Add files via upload

---
+++ Prog 1.java
@@ -0,0 +1,14 @@
+class Prog1{
+public static void main(String args[]){
+
+ String a="Noor";
+ boolean b=false;
+ int c=2;
+ double d=4.9d;
+ float e= 4.99f;
+ char f='N';
+ System.out.println(a+" "+b+" "+c+" "+d+" "+d+" "+e+" "+f+" ");
+
+}
+
+}
